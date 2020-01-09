f0060ff6dc069bb9d6391ccbecb706cccdd7fcd1
Mykola Niemtsov <mykola.niemtsov@tomtom.com>
2020-01-09 14:24:44 +0100

    Refactor Day 4

--- 2019/Day-4/main.cpp
+++ 2019/Day-4/main.cpp
@@ -2,74 +2,50 @@
 
 using namespace std;
 
-bool checkAdjustentDigits(std::string number)
+//#define PART1
+
+bool checkDigitsRules(const std::string& number)
 {
+  bool adjacentOk = false;
   for(int i = 0; i < number.size()-1; ++i)
   {
-    if(number[i] == number[i+1])
-    {
-      bool ok = true;
-      if(i > 0)
-      {
-        if(number[i-1] != number[i])
-        {
-          ok &= true;
-        }
-        else
-        {
-          continue;
-        }
-      }
-      if(ok && (i<number.size()-2))
-      {
-        if(number[i+1] != number[i+2])
-        {
-          ok &= true;
-        }
-        else
-        {
-          continue;
-        }
-      }
-      if(ok) return true;
-    }
-  }
-  return false;
-}
-
-bool checkNeverDecrease(std::string number)
-{
-  for(int i = 0; i < number.size()-1; ++i)
-  {
-    std::string s1;
-    s1+= number[i];
-
-    std::string s2;
-    s2 += number[i+1];
-
-    if(std::stoi(s1) > std::stoi(s2))
+    //check non-decreasing
+    if(number[i] > number[i+1])
     {
       return false;
     }
+
+    //check adjacent digits
+    if(!adjacentOk && (number[i] == number[i+1]))
+    {
+#ifndef PART1
+      if(((i > 0)               && (number[i-1] == number[i]  )) ||
+         ((i < number.size()-2) && (number[i+1] == number[i+2])))
+      {
+        continue;
+      }
+#endif
+      adjacentOk = true;
+    }
   }
-  return true;
+  return adjacentOk;
 }
 
 int main()
 {
   int result = 0;
-//  for(int i = 111122; i <= 111122; ++i)
-  for(int i = 125730; i <= 579381; ++i)
+  for(int i = 125730; i <= 579381; ++i)//range from input
   {
-    bool ok = true;
-
-    ok &= checkAdjustentDigits(std::to_string(i));
-    if(!ok) continue;
-
-    ok &= checkNeverDecrease(std::to_string(i));
-
-    if(ok) result++;
+    if(checkDigitsRules(std::to_string(i)))
+    {
+      result++;
+    }
   }
-  std::cout<<result<<std::endl;
+
+#ifdef PART1
+  std::cout<<"[Part 1] Answer: "<<result<<std::endl;
+#else
+  std::cout<<"[Part 2] Answer: "<<result<<std::endl;
+#endif
   return 0;
 }
