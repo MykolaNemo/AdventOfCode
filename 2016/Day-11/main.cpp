b1ad5c5841ce5ef19c9a11a811d4d494e27a917e
Mykola Niemtsov <mykola.niemtsov@gmail.com>
2020-11-01 21:10:42 +0100

    Finished Day 11, year 2016

--- 2016/Day-11/main.cpp
+++ 2016/Day-11/main.cpp
@@ -5,35 +5,19 @@
 #include <map>
 #include <set>
 #include <algorithm>
-#include <functional>
 
-/*
- * The first floor contains
- * a strontium generator,
- * a strontium-compatible microchip,
- * a plutonium generator,
- * and a plutonium-compatible microchip.
-*/
+//#define EXAMPLE
+//#define PART1
 
-/*
- * The second floor contains
- * a thulium generator,
- * a ruthenium generator,
- * a ruthenium-compatible microchip,
- * a curium generator,
- * and a curium-compatible microchip.
-*/
-
-/*
- * The third floor contains
- * a thulium-compatible microchip.
-*/
-
-/*
- * The fourth floor contains
- *  nothing relevant.
-*/
-
+#ifdef EXAMPLE
+enum class Type
+{
+    H,
+    L
+};
+const std::vector<Type> types = {Type::H, Type::L};
+#else
+#ifdef PART1
 enum class Type
 {
     S,
@@ -42,49 +26,351 @@ enum class Type
     C,
     T
 };
+const std::vector<Type> types = {Type::S, Type::P, Type::R, Type::C, Type::T};
+#else
+enum class Type
+{
+    S,
+    P,
+    R,
+    C,
+    T,
+    E,
+    D
+};
+const std::vector<Type> types = {Type::S, Type::P, Type::R, Type::C, Type::T, Type::E,  Type::D};
+#endif
+#endif
+
+enum class Object
+{
+    Generator,
+    Module
+};
 
 struct Item
 {
-    Item(int obj_, Type type_):obj(obj_),type(type_){}
-
-    int obj = 0; //0 - generator, 1 - module
-    Type type = Type::S;
+    Item(Object obj_, Type type_):obj(obj_),type(type_){}
+    bool isGenerator() const
+    {
+        return obj == Object::Generator;
+    }
+    bool isModule() const
+    {
+        return obj == Object::Module;
+    }
+    friend bool operator==(const Item& rhs, const Item& lhs)
+    {
+        return (rhs.obj == lhs.obj) && (rhs.type == lhs.type);
+    }
+    Object obj = Object::Generator;
+    Type type = static_cast<Type>(0);
 };
 
-/*
- * F3:
- * F2: TM
- * F1: RG RM CG CM TG
- * F0: SG SM PG PM
-*/
-
-auto lambda = []()->std::vector<std::vector<Item>>
+using Items = std::vector<Item>;
+using Building = std::map<int, Items>;
+std::multiset<std::pair<int, int>> buildingImage(const Building& building)
 {
-    std::vector<std::vector<Item>> floors;
-    std::vector<Item> floor1 = {Item(0,Type::S),
-                                Item(1,Type::S),
-                                Item(0,Type::P),
-                                Item(1,Type::P)};
+    std::multiset<std::pair<int, int>> image;
+    for(const Type& type : types)
+    {
+        std::pair<int, int> pair = {-1, -1};
+        for(int i = 0; i < static_cast<int>(building.size()); ++i)
+        {
+            for(const Item& item : building.at(i))
+            {
+                if(item.isGenerator() && item.type == type)
+                {
+                    pair.first = i;
+                }
+                else if(item.isModule() && item.type == type)
+                {
+                    pair.second = i;
+                }
+                if(pair.first != -1 && pair.second != -1)
+                {
+                    break;
+                }
+            }
+            if(pair.first != -1 && pair.second != -1) break;
+        }
+        if(!(pair.first == -1 && pair.second == -1))
+        {
+            image.insert(pair);
+        }
+    }
+    return image;
+}
 
-    std::vector<Item> floor2 = {Item(0,Type::R),
-                                Item(1,Type::R),
-                                Item(0,Type::C),
-                                Item(1,Type::C),
-                                Item(0,Type::T)};
+struct State
+{
+    Building building;
+    int currentFloor = 0;
 
-    std::vector<Item> floor3 = {Item(1,Type::T)};
+    //for storage
+    int steps = 0;
+    std::vector<Item> loadedItems;
 
-    std::vector<Item> floor4;
-
-    floors.push_back(floor1);
-    floors.push_back(floor2);
-    floors.push_back(floor3);
-    floors.push_back(floor4);
-    return floors;
+    bool operator==(const State& other) const
+    {
+        return ((this->currentFloor == other.currentFloor) &&
+                (buildingImage(this->building) == buildingImage(other.building)));
+    }
+    bool isFried()
+    {
+        bool hasGenerators = false;
+        for(const Item& item1 : building[currentFloor])
+        {
+            if(item1.isModule())
+            {
+                bool isShielded = false;
+                for(const Item& item2 : building[currentFloor])
+                {
+                    if(item2 == item1) continue;
+                    if(item2.isGenerator())
+                    {
+                        hasGenerators = true;
+                        if(item2.type == item1.type)
+                        {
+                            isShielded = true;
+                            break;
+                        }
+                    }
+                }
+                if(!isShielded && hasGenerators) return true;
+            }
+        }
+        return false;
+    }
 };
-std::vector<std::vector<Item>> floors = lambda();
+
+#ifdef EXAMPLE
+int NumberOfItems = 4;
+auto startBuilding = []()->auto
+{
+    Items floor1 = {Item(Object::Module, Type::H),
+                    Item(Object::Module, Type::L)};
+
+    Items floor2 = {Item(Object::Generator, Type::H)};
+
+    Items floor3 = {Item(Object::Generator, Type::L)};
+
+    Items floor4 = {};
+
+    Building building;
+    building[0] = floor1;
+    building[1] = floor2;
+    building[2] = floor3;
+    building[3] = floor4;
+    return building;
+}();
+
+#else
+#ifdef PART1
+int NumberOfItems = 10;
+auto startBuilding = []()->auto
+{
+    Items floor1 = {Item(Object::Generator, Type::S),
+                    Item(Object::Module, Type::S),
+                    Item(Object::Generator, Type::P),
+                    Item(Object::Module, Type::P)};
+
+    Items floor2 = {Item(Object::Generator, Type::R),
+                    Item(Object::Module, Type::R),
+                    Item(Object::Generator, Type::C),
+                    Item(Object::Module, Type::C),
+                    Item(Object::Generator, Type::T)};
+
+    Items floor3 = {Item(Object::Module, Type::T)};
+
+    Items floor4;
+
+    Building building;
+    building[0] = floor1;
+    building[1] = floor2;
+    building[2] = floor3;
+    building[3] = floor4;
+    return building;
+}();
+#else
+int NumberOfItems = 14;
+auto startBuilding = []()->auto
+{
+    Items floor1 = {Item(Object::Generator, Type::S),
+                    Item(Object::Module, Type::S),
+                    Item(Object::Generator, Type::P),
+                    Item(Object::Module, Type::P),
+                    Item(Object::Generator, Type::E),
+                    Item(Object::Module, Type::E),
+                    Item(Object::Generator, Type::D),
+                    Item(Object::Module, Type::D)};
+
+    Items floor2 = {Item(Object::Generator, Type::R),
+                    Item(Object::Module, Type::R),
+                    Item(Object::Generator, Type::C),
+                    Item(Object::Module, Type::C),
+                    Item(Object::Generator, Type::T)};
+
+    Items floor3 = {Item(Object::Module, Type::T)};
+
+    Items floor4;
+
+    Building building;
+    building[0] = floor1;
+    building[1] = floor2;
+    building[2] = floor3;
+    building[3] = floor4;
+    return building;
+}();
+#endif
+#endif
+
+template<class T>
+bool contains(const std::vector<T>& v, const T& item)
+{
+    return std::find(v.begin(), v.end(), item) != v.end();
+}
+
+std::vector<State> load(const State& state)
+{
+    std::vector<State> loadStates;
+    for(const Item& item1 : state.building.at(state.currentFloor))
+    {
+        State s1 = state;
+        auto& floor1 = s1.building.at(s1.currentFloor);
+        floor1.erase(std::remove(floor1.begin(), floor1.end(), item1), floor1.end());
+        s1.loadedItems.push_back(item1);
+        if(!s1.isFried() && !contains<State>(loadStates, s1))
+        {
+            loadStates.push_back(s1);
+        }
+
+        for(const Item& item2 : floor1)
+        {
+            State s2 = s1;
+            auto& floor2 = s2.building.at(s2.currentFloor);
+            floor2.erase(std::remove(floor2.begin(), floor2.end(), item2), floor2.end());
+            s2.loadedItems.push_back(item2);
+            if(!s2.isFried() && !contains<State>(loadStates, s2))
+            {
+                loadStates.push_back(s2);
+            }
+        }
+    }
+
+    {
+        std::map<std::pair<int, int>, bool> pairs = {{{1, 0}, false},
+                                                     {{0, 1}, false},
+                                                     {{1, 1}, false}};
+        for(State& loadState : loadStates)
+        {
+            std::pair<int, int> pair = {0,0};
+            for(Item& item : loadState.loadedItems)
+            {
+                if(item.isGenerator())
+                {
+                    pair.first = 1;
+                }
+                else if(item.isModule())
+                {
+                    pair.second = 1;
+                }
+            }
+            if(!pairs[pair])
+            {
+                pairs[pair] = true;
+            }
+            else
+            {
+                loadStates.erase(std::remove(loadStates.begin(), loadStates.end(), loadState), loadStates.end());
+            }
+        }
+    }
+    return loadStates;
+}
+
+std::vector<State> goUpOrDown(const State& state)
+{
+    std::vector<State> states;
+    if(state.currentFloor < 3)
+    {
+        State stateUp = state;
+        stateUp.currentFloor++;
+        stateUp.steps++;
+        states.push_back(stateUp);
+    }
+    if(state.currentFloor > 0)
+    {
+        bool dontGoDown = true;
+        for(int i = 0; i < state.currentFloor; ++i)
+        {
+            if(!state.building.at(i).empty())
+            {
+                dontGoDown = false;
+                break;
+            }
+        }
+        if(!dontGoDown)
+        {
+            State stateDown = state;
+            stateDown.currentFloor--;
+            stateDown.steps++;
+            states.push_back(stateDown);
+        }
+    }
+    return states;
+}
+
+bool unload(State& currentState)
+{
+    Items& floor = currentState.building[currentState.currentFloor];
+    for(const Item& item : currentState.loadedItems)
+    {
+        floor.push_back(item);
+    }
+    currentState.loadedItems.clear();
+    if(!currentState.isFried())
+    {
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
+    std::map<int, std::vector<State>> allStates;
+
+    std::vector<State> states;
+    {
+        State startState;
+        startState.building = startBuilding;
+        states.push_back(startState);
+    }
+    while(!states.empty())
+    {
+        State oldState = states[0];
+        states.erase(std::remove(states.begin(), states.end(), oldState), states.end());
+        for(const auto& loadState : load(oldState))
+        {
+            for(auto& state : goUpOrDown(loadState))
+            {
+                if(unload(state))
+                {
+                    if(static_cast<int>(state.building.at(3).size()) == NumberOfItems)
+                    {
+                        std::cout<<"Answer: "<<state.steps<<std::endl;
+                        return 0;
+                    }
+                    int currentFloor = state.currentFloor;
+                    auto allIt = std::find(allStates[currentFloor].begin(), allStates[currentFloor].end(), state);
+                    if(allIt == allStates[currentFloor].end())
+                    {
+                        allStates[currentFloor].push_back(state);
+                        states.push_back(state);
+                    }
+                }
+            }
+        }
+    }
     return 0;
 }
